e10a5723cef30549af08414d6fd73b3a81db5496
zelabbas <zakariaelabbassi2002@gmail.com>
2024-05-31 18:39:24 +0100

    free all data that were allocated

---
+++ malloc/free_data.c
@@ -0,0 +1,24 @@
+#include "../include/cub3d.h"
+
+void	ft_free_matirx(char **matrix)
+{
+	int i;
+
+	if (!(*matrix) || !matrix)
+		return ;
+	i = -1;
+	while (matrix[++i])
+		free(matrix[i]);
+	free(matrix);
+}
+
+void	ft_free_data(t_cub *cub)
+{
+	free(cub->no);
+	free(cub->so);
+	free(cub->we);
+	free(cub->ea);
+	free(cub->cf);
+	free(cub->cc);
+	ft_free_matirx(cub->map);
+}

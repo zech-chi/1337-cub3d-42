7d83405e5bea06debc589e08a762ec6b988efa91
zelabbas <zakariaelabbassi2002@gmail.com>
2024-06-02 09:39:55 +0100

    a small update

--- main.c
+++ main.c
@@ -6,14 +6,15 @@
 /*   By: zelabbas <zelabbas@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 12:01:31 by zech-chi          #+#    #+#             */
-/*   Updated: 2024/06/01 15:51:53 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/06/01 21:08:03 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/cub3d.h"
-
+void leak(){system("leaks cub3D");}
 int	main(int ac, char **av)
 {
+	atexit(leak);
 	t_cub	cub;
 
 	ft_parsing(ac, av, &cub);
@@ -21,5 +22,6 @@ int	main(int ac, char **av)
 	ft_display_list(cub.head);
 	printf("\n--------------------------------\n");
 	printf("rows:%d, colos:%d", cub.rows, cub.cols);
+	ft_free_data(&cub);
 	return (0);
 }
